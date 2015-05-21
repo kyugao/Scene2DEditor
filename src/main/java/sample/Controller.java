63f3a7575beccfbc58b4e36bdb53e8c78882a0c8
Adam Gao <26335804@qq.com>
2015-05-21 18:51:48 +0800

    update ui

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TreeView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class Controller {
+
+    @FXML protected TreeView resourceTree;
+
+    @FXML protected void onAddClick() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        File selectedFile = fileChooser.showOpenDialog(Main.currentStage);
+        loadResources(selectedFile);
+    }
+
+    @FXML protected void onDelClick() {
+        System.out.println("del");
+    }
+
+    private void loadResources(File file) {
+        System.out.println(file.getAbsolutePath());
+    }
+
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -8,14 +8,16 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public static Stage currentStage;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        currentStage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("/main-ui.fxml"));
         primaryStage.setTitle("Libgdx Scene2D UI Editor");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
